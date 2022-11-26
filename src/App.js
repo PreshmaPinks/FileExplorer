40fe5a62470027fec0a31e91eb9286e70ae19e8a
PreshmaPinks <pinkypreshma@gmail.com>
2022-11-27 01:24:37 +0530

    added folder and file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import "./App.css";
+import { explorer } from "./data/folderData";
+import Folder from "./components/Folder";
 
 function App() {
+  const [explorerData, setExplorerData] = useState(explorer);
+  console.log("explorerData", explorerData);
   return (
     <div className="App">
       <h1>File Explorer App</h1>
+      <Folder explorer={explorerData} />
     </div>
   );
 }
